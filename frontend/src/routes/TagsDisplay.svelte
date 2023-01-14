bca184f1023231f202f5ee5f42dbe46a6a75341e
Vincent <vincentvibe3@gmail.com>
2023-01-14 11:28:24 -0500

    Finalize addBook page

--- frontend/src/routes/TagsDisplay.svelte
+++ frontend/src/routes/TagsDisplay.svelte
@@ -21,7 +21,12 @@
 <div class="container">
 	{#each tags as tag}
 		{#if selectedTags.includes(tag)}
-			<Button style="margin:0.2rem;" type="secondary" on:click={()=>{removeTag(tag)}}>Remove {tag}</Button>
+			<Button style="margin:0.2rem; height:2.25rem;" type="secondary" on:click={()=>{removeTag(tag)}}>
+				<slot slot="icon">
+					<svg xmlns="http://www.w3.org/2000/svg" width="192" height="192" viewBox="0 0 256 256"><rect width="256" height="256" stroke="none" fill="none"></rect><line x1="200" y1="56" x2="56" y2="200" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="200" y1="200" x2="56" y2="56"stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line></svg>
+				</slot>
+				{tag}
+			</Button>
 		{:else }
 			<Button style="margin:0.2rem;" on:click={()=>{addTag(tag)}}>{tag}</Button>
 		{/if}
@@ -34,5 +39,9 @@
 		flex-wrap: wrap;
 		align-items: center;
 		justify-content: center;
+	}
+	.container:global( svg ) {
+		fill: var(--btnSecondaryIcon);
+		stroke: var(--btnSecondaryIcon);
 	}
 </style>
